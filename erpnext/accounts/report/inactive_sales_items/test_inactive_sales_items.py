b06486db98ce9142e43c205c148963d83779af34
sujaypaul8848 <sujay.paul@8848digital.com>
2025-09-15 14:59:21 +0530

    fix: test_get_territories_with_and_without_filter_TC_ACC_417.<locals>.<lambda>() got an unexpected keyword argument pluck

--- erpnext/accounts/report/inactive_sales_items/test_inactive_sales_items.py
+++ erpnext/accounts/report/inactive_sales_items/test_inactive_sales_items.py
@@ -111,12 +111,12 @@ class TestInactiveSalesItems(FrappeTestCase):
 
 	def test_get_territories_with_and_without_filter_TC_ACC_417(self):
 		filters = self._mk_filters(territory="India")
-		frappe.get_all = lambda doctype, fields, filters: [SimpleNamespace(name="India")]
+		frappe.get_all = lambda *args, **kwargs: [SimpleNamespace(name="India")]
 		result = report.get_territories(filters)
 		self.assertEqual(result[0].name, "India")
 
 		filters = self._mk_filters()
-		frappe.get_all = lambda doctype, fields, filters: [SimpleNamespace(name="USA")]
+		frappe.get_all = lambda *args, **kwargs: [SimpleNamespace(name="India")]
 		result = report.get_territories(filters)
 		self.assertEqual(result[0].name, "USA")
 
